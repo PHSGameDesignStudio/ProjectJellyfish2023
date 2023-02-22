5ed7388dfebbae30804f9bdfe6ada3bc6cb848d3
ShelfMaster27 <90055929+ShelfMaster27@users.noreply.github.com>
2023-02-21 21:38:21 -0500

    Stel will now not phase through walls- yay

---
+++ Player.gd
@@ -0,0 +1,50 @@
+extends KinematicBody2D
+
+var velocity
+var _animated_sprite
+var last_action_pressed 
+export var speed = 100
+
+func get_input():
+	velocity = Vector2()
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+		last_action_pressed = "right"
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+		last_action_pressed = "left"
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+		last_action_pressed = "down"
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+		last_action_pressed = "up"
+	velocity = velocity.normalized() * 150
+	_animated_sprite = $AnimatedSprite
+	
+func _process(_delta):
+	if Input.is_action_pressed("ui_right"):
+		_animated_sprite.flip_h = false
+		_animated_sprite.play("left_right")
+	elif Input.is_action_pressed("ui_left"):
+		_animated_sprite.flip_h = true
+		_animated_sprite.play("left_right")
+	elif Input.is_action_pressed("ui_up"):
+		_animated_sprite.play("backward")
+	elif Input.is_action_pressed("ui_down"):
+		_animated_sprite.play("forward")
+	elif velocity.x == 0 and last_action_pressed == "right":
+		_animated_sprite.flip_h = false
+		_animated_sprite.play("idle left_right")
+	elif velocity.x == 0 and last_action_pressed == "left":
+		_animated_sprite.flip_h = true
+		_animated_sprite.play("idle left_right")
+	elif velocity.x == 0 and last_action_pressed == "down":
+		_animated_sprite.play("idle forward")
+	elif velocity.x == 0 and last_action_pressed == "up":
+		_animated_sprite.play("idle backward")
+
+func _physics_process(_delta):
+	get_input()
+	move_and_slide(velocity)
+	
