1a1cf7194dd30ebfced99296bd3ba8876c55389a
ShelfMaster27 <90055929+ShelfMaster27@users.noreply.github.com>
2023-02-26 14:06:25 -0500

    camera fixed + coral

--- SceneChangeTrigger.gd
+++ SceneChangeTrigger.gd
@@ -12,3 +12,8 @@ func _process(delta):
 func _on_SceneTrigger_body_entered(body):
 	if (body.name == "Player"):
 		get_tree().change_scene(nextScene)
+
+
+func _on_SceneTrigger_body_exited(body):
+	if (body.name == "Player"):
+		get_tree().change_scene(nextScene)
--- World.gd
+++ World.gd
@@ -5,17 +5,27 @@ var entered = false
 
 export (String) var world_name = "World"
 
-export (float) var topLimit = -1000;
-export (float) var bottomLimit = -1000;
-export (float) var rightLimit = -1000;
-export (float) var leftLimit = -1000;
 
 func _ready():
-	var cam = $Player/Camera2D
-	cam.limit_left = leftLimit;
-	cam.limit_right = rightLimit;
-	cam.limit_top = topLimit;
-	cam.limit_bottom = bottomLimit;
+	var cam = get_node("Player/Camera2D")
+	if world_name == "Starting Cave":
+		var map_limits = get_node("TileMapStartingCave").get_used_rect()
+		var map_cellsize = get_node("TileMapStartingCave").cell_size
+		cam.zoom.x = 0.5
+		cam.zoom.y = 0.575
+		cam.limit_left = map_limits.position.x  * map_cellsize.x
+		cam.limit_top = map_limits.position.y * map_cellsize.y
+		cam.limit_right = map_limits.end.x * map_cellsize.x
+		cam.limit_bottom = map_limits.end.y * map_cellsize.y
+	elif world_name == "Sea Cave 1":
+		var map_limits = get_node("SeaCave1TileMap").get_used_rect()
+		var map_cellsize = get_node("SeaCave1TileMap").cell_size
+		cam.zoom.x = 1
+		cam.zoom.y = 1
+		cam.limit_left = map_limits.position.x  * map_cellsize.x
+		cam.limit_top = map_limits.position.y * map_cellsize.y
+		cam.limit_right = map_limits.end.x * map_cellsize.x
+		cam.limit_bottom = map_limits.end.y * map_cellsize.y
 #func _process(delta):
 	#if entered:
 		
