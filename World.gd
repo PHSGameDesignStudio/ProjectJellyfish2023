d96e8c1fac95546bd188e1f2de97aa121678224a
ShelfMaster27 <90055929+ShelfMaster27@users.noreply.github.com>
2023-03-05 15:44:44 -0500

    added Goose and Tom

--- World.gd
+++ World.gd
@@ -1,11 +1,11 @@
 extends Node2D
 
 
-export (String) var world_name = 
+export (String) var world_name = name
 
 func _ready():
 	var cam = get_node("Player/Camera2D")
-	if world_name == :
+	if world_name == "Starting Cave":
 		var map_limits = get_node("TileMapStartingCave").get_used_rect()
 		var map_cellsize = get_node("TileMapStartingCave").cell_size
 		cam.zoom.x = 0.5
@@ -14,7 +14,7 @@ func _ready():
 		cam.limit_top = map_limits.position.y * map_cellsize.y
 		cam.limit_right = map_limits.end.x * map_cellsize.x
 		cam.limit_bottom = map_limits.end.y * map_cellsize.y
-	elif world_name == :
+	elif world_name == "Sea Cave 1":
 		var map_limits = get_node("SeaCave1TileMap").get_used_rect()
 		var map_cellsize = get_node("SeaCave1TileMap").cell_size
 		cam.zoom.x = 1
@@ -23,11 +23,12 @@ func _ready():
 		cam.limit_top = map_limits.position.y * map_cellsize.y
 		cam.limit_right = map_limits.end.x * map_cellsize.x
 		cam.limit_bottom = map_limits.end.y * map_cellsize.y
-	elif world_name == :
+	elif world_name == "Amons Cave":
+		print ("hi")
 		var map_limits = get_node("TileMapAmon'sCave").get_used_rect()
 		var map_cellsize = get_node("TileMapAmon'sCave").cell_size
-		cam.zoom.x = 0.5
-		cam.zoom.y = 0.575
+		cam.zoom.x = 0.1
+		cam.zoom.y = 0.1
 		cam.limit_left = map_limits.position.x  * map_cellsize.x
 		cam.limit_top = map_limits.position.y * map_cellsize.y
 		cam.limit_right = map_limits.end.x * map_cellsize.x
