e176d976beb9979494b50343cf9ddf0da8b60c4c
ShelfMaster27 <90055929+ShelfMaster27@users.noreply.github.com>
2023-03-05 18:50:55 -0500

    attempt to fix scene change system

--- Project Jellyfish.gd
+++ Project Jellyfish.gd
@@ -1,4 +1,8 @@
 extends Node2D
 
-
+var world_name = GlobalWorld.world_name
 export var scene_changed = false
+
+func _ready():
+	if get_tree().change_scene()
+		scene_changed = true
--- SceneChangeTrigger.gd
+++ SceneChangeTrigger.gd
@@ -2,29 +2,22 @@ extends Area2D
 
 onready var anim_player = $SceneChangePlayer
 
-func _ready():
-	if $"/root/ProjectJellyfish".scene_changed:
+func _process(_delta):
+	if $"/root/ProjectJellyfish".scene_changed == true:
 		anim_player.play("FadeIn")
 		anim_player.play("FadeOut")
-		yield(anim_player, "animation_finished")
 		$"/root/ProjectJellyfish".scene_changed = false
 			
 func _on_To_Amons_Cave_body_entered(body):
 	if (body.name == "Player"):
-		var tree = get_tree()
-		$"/root/ProjectJellyfish".scene_changed = true
-		tree.change_scene("res://Amon's Cave.tscn")
+		get_tree().change_scene("res://Amon's Cave.tscn")
 
 
 func _on_To_Sea_Cave_1_body_entered(body):
 	if (body.name == "Player"):
-		var tree = get_tree()
-		$"/root/ProjectJellyfish".scene_changed = true
-		tree.change_scene("res://Sea Cave 1.tscn")
+		get_tree().change_scene("res://Sea Cave 1.tscn")
 
 
 func _on_To_Starting_Cave_body_entered(body):
 	if (body.name == "Player"):
-		var tree = get_tree()
-		$"/root/ProjectJellyfish".scene_changed = true
-		tree.change_scene("res://Starting Cave.tscn")
+		get_tree().change_scene("res://Starting Cave.tscn")
