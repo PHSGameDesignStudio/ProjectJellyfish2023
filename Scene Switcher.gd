deae8507e280df2e8d233704630d275a15822260
ShelfMaster27 <90055929+ShelfMaster27@users.noreply.github.com>
2023-03-08 00:09:13 -0500

    potentially closer to breakthrough on scene switching

--- Scene Switcher.gd
+++ Scene Switcher.gd
@@ -1,2 +1,17 @@
 extends Node
 
+
+var next_level
+var next_level_name: String
+var current_level_name = GlobalWorld.world_name
+onready var current_level = load(current_level_name + ".tscn")
+
+func _ready() -> void:
+	add_child(current_level)
+
+func handle_level_changed(current_level_name: String):
+	next_level = load(next_level_name + ".tscn").instance()
+	add_child(next_level)
+	get_child(current_level).queue_free()
+	current_level = next_level
+
--- World.gd
+++ World.gd
@@ -1,19 +1,10 @@
 extends Node2D
 
-export (String) var world_name = name
-onready var next_level = "Sea Cave 1.tscn"
-onready var current_level = str(world_name, ".tscn")
-
-func scene_change():
-	SceneChangePlayer.play("SceneChangeFade")
-	queue_free()
-	get_tree().change_scene(next_level)
-	current_level = next_level
-	print("scene changed")
+export (String) var world_name 
+onready var current_level_name = world_name
+	
 
 func _ready():
-	current_level = "Starting Cave.tscn"
-	next_level = "Sea Cave 1.tscn"
 	var cam = get_node("Player/Camera2D")
 	if world_name == "Starting Cave":
 		var map_limits = get_node("TileMapStartingCave").get_used_rect()
@@ -42,24 +33,3 @@ func _ready():
 		cam.limit_top = map_limits.position.y * map_cellsize.y
 		cam.limit_right = map_limits.end.x * map_cellsize.x
 		cam.limit_bottom = map_limits.end.y * map_cellsize.y
-
-
-
-
-func _on_To_Amons_Cave_body_entered(body):
-	if body.name == "Player":
-		next_level = "Amon's Cave.tscn"
-		scene_change()
-
-
-func _on_To_Starting_Cave_body_entered(body):
-	if body.name == "Player":
-		next_level = "Starting Cave.tscn"
-		scene_change()
-
-
-func _on_To_Sea_Cave_1_body_entered(body):
-	if body.name == "Player":
-		next_level = "Sea Cave 1.tscn"
-		scene_change()
-
