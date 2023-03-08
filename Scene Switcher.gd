93c872a440373c54f4b5f4bf9b03fa1cb3d062b0
ShelfMaster27 <90055929+ShelfMaster27@users.noreply.github.com>
2023-03-08 11:50:09 -0500

    Possible progress on scene change

--- Scene Switcher.gd
+++ Scene Switcher.gd
@@ -4,14 +4,15 @@ extends Node
 var next_level
 var next_level_name: String
 var current_level_name = GlobalWorld.world_name
-onready var current_level = load(current_level_name + ".tscn")
+var current_level = load(str(current_level_name, ".tscn"))
 
 func _ready() -> void:
-	add_child(current_level)
+	current_level = load(current_level_name + ".tscn")
+	print(current_level)
 
-func handle_level_changed(current_level_name: String):
+func level_changed():
 	next_level = load(next_level_name + ".tscn").instance()
 	add_child(next_level)
-	get_child(current_level).queue_free()
+	get_node("Starting Cave").queue_free()
 	current_level = next_level
 
--- World.gd
+++ World.gd
@@ -1,7 +1,6 @@
 extends Node2D
 
 export (String) var world_name 
-onready var current_level_name = world_name
 	
 
 func _ready():
