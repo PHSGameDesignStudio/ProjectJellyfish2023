654afc33e061a766293344b915f1fb5371eb6987
ShelfMaster27 <90055929+ShelfMaster27@users.noreply.github.com>
2023-03-08 22:02:56 -0500

    Revert "potentially closer to breakthrough on scene switching"
    This reverts commit deae8507e280df2e8d233704630d275a15822260.

--- SceneChangeTrigger.gd
+++ SceneChangeTrigger.gd
@@ -1,18 +1,1 @@
 extends Area2D
-
-var current_level_name = GlobalWorld.world_name
-
-
-func _on_To_Sea_Cave_1_body_entered(body):
-	SceneSwitcher.next_level_name = "Sea Cave 1"
-	SceneSwitcher.handle_level_changed(current_level_name)
-
-
-func _on_To_Amons_Cave_body_entered(body):
-	SceneSwitcher.next_level_name = "Amon's Cave"
-	SceneSwitcher.handle_level_changed(current_level_name)
-
-
-func _on_To_Starting_Cave_body_entered(body):
-	SceneSwitcher.next_level_name = "Starting Cave"
-	SceneSwitcher.handle_level_changed(current_level_name)
