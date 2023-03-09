7c8db5f5bae319d72771dbd354babb3015af591d
ShelfMaster27 <90055929+ShelfMaster27@users.noreply.github.com>
2023-03-08 22:03:05 -0500

    Revert "Fix scene change animation timing"
    This reverts commit ccb06c30ddbd9a50bfc98a15f00cd426848716b3.

--- World.gd
+++ World.gd
@@ -1,19 +1,20 @@
 extends Node2D
 
 export (String) var world_name = name
-onready var next_level = "Sea Cave 1.tscn"
-onready var current_level = str(world_name, ".tscn")
+onready var next_level_name = "Sea Cave 1"
+onready var current_level_name = world_name
+onready var current_level = str(current_level_name)
+onready var next_level = str(next_level_name)
 
+	
 func scene_change():
 	SceneChangePlayer.play("SceneChangeFade")
-	queue_free()
-	get_tree().change_scene(next_level)
-	current_level = next_level
-	print("scene changed")
+	get_tree().change_scene(next_level_name)
+	current_level_name = next_level_name
 
 func _ready():
-	current_level = "Starting Cave.tscn"
-	next_level = "Sea Cave 1.tscn"
+	current_level_name = "Starting Cave"
+	next_level_name = "Sea Cave 1"
 	var cam = get_node("Player/Camera2D")
 	if world_name == "Starting Cave":
 		var map_limits = get_node("TileMapStartingCave").get_used_rect()
@@ -47,19 +48,17 @@ func _ready():
 
 
 func _on_To_Amons_Cave_body_entered(body):
-	if body.name == "Player":
-		next_level = "Amon's Cave.tscn"
-		scene_change()
+	next_level_name = "Amon's Cave"
+	scene_change()
 
 
 func _on_To_Starting_Cave_body_entered(body):
-	if body.name == "Player":
-		next_level = "Starting Cave.tscn"
-		scene_change()
+	next_level_name = "Starting Cave.tscn"
+	scene_change()
 
 
 func _on_To_Sea_Cave_1_body_entered(body):
-	if body.name == "Player":
-		next_level = "Sea Cave 1.tscn"
-		scene_change()
+	next_level_name = "res://Sea Cave 1.tscn"
+	print(next_level)
+	scene_change()
 
