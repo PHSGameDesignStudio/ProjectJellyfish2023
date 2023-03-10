2639033a3cef021c3e4b0d3b6ecefab599434f6a
ShelfMaster27 <90055929+ShelfMaster27@users.noreply.github.com>
2023-03-09 22:12:38 -0500

    trying to make the position change depending on which doorway you came from

--- SceneChangeTrigger.gd
+++ SceneChangeTrigger.gd
@@ -1,1 +1,9 @@
 extends Area2D
+
+export (String) var door
+
+
+func _ready():
+	if door == "SC1-SC":
+		PlayerAnimatedSprite.flip_h = true
+		Player.global_position(Vector2(1,11))
--- World.gd
+++ World.gd
@@ -5,6 +5,7 @@ onready var next_level_name = "Sea Cave 1"
 onready var current_level_name = world_name
 onready var current_level = str(current_level_name)
 onready var next_level = str(next_level_name)
+onready var prev_level
 
 	
 func scene_change():
@@ -12,13 +13,13 @@ func scene_change():
 	sceneChangePlayer.play("SceneChangeFade")
 	yield(get_tree().create_timer(sceneChangePlayer.current_animation_length / 2.0), "timeout")
 	get_tree().change_scene(next_level_name)
+	prev_level = current_level_name
 	current_level_name = next_level_name
 	
 
 func _ready():
-	current_level_name = "Starting Cave"
-	next_level_name = "Sea Cave 1"
-	var cam = get_node("Player/Camera2D")
+	Player.global_transform(Vector2(1,11))
+	var cam = $Player/Camera2D
 	if world_name == "Starting Cave":
 		var map_limits = get_node("TileMapStartingCave").get_used_rect()
 		var map_cellsize = get_node("TileMapStartingCave").cell_size
@@ -40,8 +41,8 @@ func _ready():
 	elif world_name == "Amon's Cave":
 		var map_limits = get_node("TileMapAmon'sCave").get_used_rect()
 		var map_cellsize = get_node("TileMapAmon'sCave").cell_size
-		cam.zoom.x = 0.1
-		cam.zoom.y = 0.1
+		cam.zoom.x = 0.5
+		cam.zoom.y = 0.575
 		cam.limit_left = map_limits.position.x  * map_cellsize.x
 		cam.limit_top = map_limits.position.y * map_cellsize.y
 		cam.limit_right = map_limits.end.x * map_cellsize.x
@@ -51,23 +52,20 @@ func _ready():
 
 
 func _on_To_Amons_Cave_body_entered(body):
-	next_level_name = "Amon's Cave"
-	scene_change()
+	if body.name == "Player":
+		next_level_name = "Amon's Cave.tscn"
+		scene_change()
 
 
 func _on_To_Starting_Cave_body_entered(body):
-	next_level_name = "Starting Cave.tscn"
-	scene_change()
+	if body.name == "Player":
+		next_level_name = "Starting Cave.tscn"
+		scene_change()
 
 
 func _on_To_Sea_Cave_1_body_entered(body):
-	next_level_name = "res://Sea Cave 1.tscn"
-	print(next_level)
-	scene_change()
+	if body.name == "Player":
+		next_level_name = "Sea Cave 1.tscn"
+		scene_change()
 
 
-
-func _on_Transition_Timer_timeout():
-	pass
-	#get_tree().change_scene(next_level_name)
-	#current_level_name = next_level_name
