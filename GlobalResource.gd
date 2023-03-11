57efed211103ac6337dd9c43f7b1bc3c9e0cddca
Roc Yu <rocyu@protonmail.com>
2023-03-11 11:01:16 -0500

    MVP for scene transition taking position into account
    Animation orientation still outstanding. Probably should make the walls and scene triggers more tidy.

---
+++ GlobalResource.gd
@@ -0,0 +1,4 @@
+class_name GlobalResource
+extends Resource
+
+export var matching_scene_trigger: String = ""
--- World.gd
+++ World.gd
@@ -1,70 +1,106 @@
 extends Node2D
 
-export (String) var world_name = name
-onready var next_level_name = "Sea Cave 1"
-onready var current_level_name = world_name
-onready var current_level = str(current_level_name)
-onready var next_level = str(next_level_name)
+var curr_scene = "Starting Cave"
+onready var globals = preload("res://GlobalResource.tres")
 
-	
-func scene_change():
-	var sceneChangePlayer : AnimationPlayer = get_viewport().get_node("SceneChangePlayer");
-	sceneChangePlayer.play("SceneChangeFade")
-	yield(get_tree().create_timer(sceneChangePlayer.current_animation_length / 2.0), "timeout")
-	get_tree().change_scene(next_level_name)
-	current_level_name = next_level_name
-	
 
 func _ready():
-	#Player.global_transform(Vector2(1,11))
+	print("New scene instance!")
+	
+	var map_limits
+	var map_cellsize
 	var cam = $Player/Camera2D
-	if world_name == "Starting Cave":
-		var map_limits = get_node("TileMapStartingCave").get_used_rect()
-		var map_cellsize = get_node("TileMapStartingCave").cell_size
+	var matching_scene_trigger = globals.matching_scene_trigger
+	
+	curr_scene = get_tree().get_current_scene().get_name()
+	
+	if curr_scene == "Starting Cave":
+		map_limits = get_node("TileMapStartingCave").get_used_rect()
+		map_cellsize = get_node("TileMapStartingCave").cell_size
 		cam.zoom.x = 0.5
 		cam.zoom.y = 0.575
-		cam.limit_left = map_limits.position.x  * map_cellsize.x
-		cam.limit_top = map_limits.position.y * map_cellsize.y
-		cam.limit_right = map_limits.end.x * map_cellsize.x
-		cam.limit_bottom = map_limits.end.y * map_cellsize.y
-	elif world_name == "Sea Cave 1":
-		var map_limits = get_node("SeaCave1TileMap").get_used_rect()
-		var map_cellsize = get_node("SeaCave1TileMap").cell_size
+	elif curr_scene == "Sea Cave 1":
+		map_limits = get_node("SeaCave1TileMap").get_used_rect()
+		map_cellsize = get_node("SeaCave1TileMap").cell_size
 		cam.zoom.x = 1
 		cam.zoom.y = 1
-		cam.limit_left = map_limits.position.x  * map_cellsize.x
-		cam.limit_top = map_limits.position.y * map_cellsize.y
-		cam.limit_right = map_limits.end.x * map_cellsize.x
-		cam.limit_bottom = map_limits.end.y * map_cellsize.y
-	elif world_name == "Amon's Cave":
-		var map_limits = get_node("TileMapAmon'sCave").get_used_rect()
-		var map_cellsize = get_node("TileMapAmon'sCave").cell_size
+	elif curr_scene == "Amon's Cave":
+		map_limits = get_node("TileMapAmon'sCave").get_used_rect()
+		map_cellsize = get_node("TileMapAmon'sCave").cell_size
 		cam.zoom.x = 0.5
 		cam.zoom.y = 0.575
-		cam.limit_left = map_limits.position.x  * map_cellsize.x
-		cam.limit_top = map_limits.position.y * map_cellsize.y
-		cam.limit_right = map_limits.end.x * map_cellsize.x
-		cam.limit_bottom = map_limits.end.y * map_cellsize.y
+	else:
+		pass
+	
+	cam.limit_left = map_limits.position.x  * map_cellsize.x
+	cam.limit_top = map_limits.position.y * map_cellsize.y
+	cam.limit_right = map_limits.end.x * map_cellsize.x
+	cam.limit_bottom = map_limits.end.y * map_cellsize.y
+	
+	var scene_trigger = get_node(matching_scene_trigger)
+	# FIXME: this check is probably indicative of poor design
+	if scene_trigger != null:
+		var position = scene_trigger.get_child(0).global_position
+		var mid_x = (cam.limit_right - cam.limit_left) / 2
+		var dev_x = (position.x - mid_x) / mid_x
+		if dev_x > 0:
+			position.x -= 100
+			$Player.global_position = position
+		elif dev_x < 0:
+			position.x += 100
+			$Player.global_position = position
+	else:
+		pass
+	
+	register_scene_triggers()
 
+func _notification(notif: int):
+	match notif:
+		MainLoop.NOTIFICATION_WM_QUIT_REQUEST, MainLoop.NOTIFICATION_WM_GO_BACK_REQUEST:
+			# FIXME: probably a cleaner way to do this
+			globals.matching_scene_trigger = ""
+			ResourceSaver.save("res://GlobalResource.tres", globals)
+		_:
+			pass
 
+func check_scene_change(colliding_body, scene_trigger):
+	if colliding_body.name == "Player":
+		scene_change(scene_trigger.get_name())
 
+func scene_change(scene_trigger):
+	var sceneChangePlayer: AnimationPlayer = get_viewport().get_node("SceneChangePlayer");
+	sceneChangePlayer.play("SceneChangeFade")
+	yield(get_tree().create_timer(sceneChangePlayer.current_animation_length / 2.5), "timeout")
+	
+	var next_scene
+	match scene_trigger.rfind("(") - 1:
+		-2:
+			next_scene = scene_trigger.substr(3)
+		var n:
+			next_scene = scene_trigger.substr(3, n - 3)
+	var next_scene_path = str(next_scene, ".tscn")
+	var matching_scene_trigger = scene_trigger.replace(next_scene, curr_scene)
+	globals.matching_scene_trigger = matching_scene_trigger
+	ResourceSaver.save("res://GlobalResource.tres", globals)
+	
+	get_tree().change_scene(next_scene_path)
 
-func _on_To_Amons_Cave_body_entered(body):
-	if body.name == "Player":
-		next_level_name = "Amon's Cave.tscn"
-		scene_change()
-
-
-func _on_To_Starting_Cave_body_entered(body):
-	if body.name == "Player":
-		next_level_name = "Starting Cave.tscn"
-		scene_change()
-
-
-func _on_To_Sea_Cave_1_body_entered(body):
-	if body.name == "Player":
-		next_level_name = "res://Sea Cave 1.tscn"
-		scene_change()
-
-
-
+# has to be manually updated... (FIXME?)
+func register_scene_triggers():
+	var scene_trigger
+	
+	# looking like yandev right now :(
+	# ideal: find a better way to coalesce signals
+	if curr_scene == "Starting Cave":
+		scene_trigger = get_node("To Sea Cave 1")
+		scene_trigger.connect("body_entered", self, "check_scene_change", [scene_trigger])
+	elif curr_scene == "Sea Cave 1":
+		scene_trigger = get_node("To Starting Cave")
+		scene_trigger.connect("body_entered", self, "check_scene_change", [scene_trigger])
+		scene_trigger = get_node("To Amon's Cave")
+		scene_trigger.connect("body_entered", self, "check_scene_change", [scene_trigger])
+	elif curr_scene == "Amon's Cave":
+		scene_trigger = get_node("To Sea Cave 1")
+		scene_trigger.connect("body_entered", self, "check_scene_change", [scene_trigger])
+	else:
+		pass
