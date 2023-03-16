ef0b871b6de4f762028ceffe5a4d923a15173ecb
Roc Yu <rocyu@protonmail.com>
2023-03-16 12:34:02 -0400

    Solely have player instance in Project Jellyfish node

--- Project Jellyfish.gd
+++ Project Jellyfish.gd
@@ -3,3 +3,5 @@ extends Node2D
 
 func _ready():
 	print("Does this print statement ever execute?")
+	var start = preload("Starting Cave.tscn")
+	add_child_below_node(get_node("SceneChangePlayer"), start.instance())
