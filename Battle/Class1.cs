ef83b612eb85754f428cae0f99a309fb357c787a
SkyanSam <samuelwiggins15@gmail.com>
2023-04-05 19:25:04 -0400

    battle wip

---
+++ Battle/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Godot;
+public class HPBox : Node
+{
+    public void SetHP(float hp, float totalhp)
+    {
+
+    }
+}
