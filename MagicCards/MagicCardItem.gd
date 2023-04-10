28fa82311b1f7094c1c3076a8d802152c9b5f020
SkyanSam <samuelwiggins15@gmail.com>
2023-04-10 14:59:43 -0400

    add sample spell assets

--- MagicCards/MagicCardItem.gd
+++ MagicCards/MagicCardItem.gd
@@ -1,6 +1,13 @@
 extends Resource
 class_name Magic_Card_Item
+const Element = {
+	EARTH = 0,
+	AIR = 1,
+	FIRE = 2,
+	WATER = 3
+}
 export (String) var spell_name
 export (String) var spell_description
-export (int) var element
-export (Array) var incantation
+export (Element) var element
+export (PoolIntArray) var incantation
+export (Texture) var spell_card_image
